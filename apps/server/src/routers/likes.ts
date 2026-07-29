3ae037e4012ba7f7e81ad9695eb09d87d7122c01
ayupandey12 <ayushpandey9802@gmail.com>
2026-07-29 21:34:26 +0530

    added users router and likes

--- apps/server/src/routers/likes.ts
+++ apps/server/src/routers/likes.ts
@@ -4,4 +4,20 @@ import { Router,Response,Request,NextFunction } from "express"
 
 const router:Router=Router()
 
-router.post("/like")
+router.post("/like",async(req:Request,res:Response,next:NextFunction)=>{
+    const {post_id}=req.body
+    //@ts-ignore
+    const user_id =req.user_id
+    try {
+        const like=await prisma.like.create({
+            data:{
+                post_id:post_id,
+                user_id:user_id
+            }
+        })
+        return res.json({success:true,like})
+    } catch (error) {
+        next(error)
+    }
+})
+export default router
